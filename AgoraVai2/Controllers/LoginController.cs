21938ae523fac084e140d5843ea491d86244d98b
Matheus Infante <matheus-infante@hotmail.com>
2024-02-28 15:46:38 -0300

    projetinho

---
+++ AgoraVai2/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using AgoraVai2.ViewModel;
+using Entidade.Models;
+using Entidade;
+using Microsoft.AspNetCore.Mvc;
+using Negocio.Cadastro;
+using DnsClient;
+
+namespace AgoraVai2.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IIndicadoDbSettings _settings;
+
+        public LoginController(ILogger<HomeController> logger, IIndicadoDbSettings settings)
+        {
+            _logger = logger;
+            _settings = settings;
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Logar(LogarViewModel model)
+        {
+            if (model.login =="adm" && model.senha == "adm")
+            {
+      
+                return RedirectToAction("Index", "home");
+            }
+
+
+            return View("Login");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
--- AgoraVai2/obj/Debug/net7.0/AgoraVai2.AssemblyInfo.cs
+++ AgoraVai2/obj/Debug/net7.0/AgoraVai2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgoraVai2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224072931994cdb455278c27637ba514af5b5d91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23727464a4f4a7bec800eaec3640ba93d7090d35")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgoraVai2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgoraVai2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
